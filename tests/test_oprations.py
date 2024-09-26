a7a7d80831416de03f876e7bc25f50b63a40ea23
Anirudh Pradhan <anipradhan.04@gmail.com>
2024-09-26 12:19:05 +0530

    new update

--- tests/test_oprations.py
+++ tests/test_oprations.py
@@ -1,16 +1,16 @@
 from src.math_operation import add,sub
 
 def test_add():
-    assert(2,3)==5
-    assert(7,2)==9
-    assert(0,0)==0
-    assert(-2,3)==1
-    assert(2,-3)==-1
+    assert add(2,3)==5
+    assert add(7,2)==9
+    assert add(0,0)==0
+    assert add(-2,3)==1
+    assert add(2,-3)==-1
 
 
 def test_sub():
-    assert(7,4)==3
-    assert(0,0)==0
-    assert(-7,4)==-11
-    assert(7,-4)==11
-    assert(3,4)==-1
+    assert sub(7,4)==3
+    assert sub(0,0)==0
+    assert sub(-7,4)==-11
+    assert sub(7,-4)==11
+    assert sub(3,4)==-1
